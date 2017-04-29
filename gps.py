c1e99246d541e7c49aab6cbcaf31291f7216184d
arahatashun <arahatashun@gmail.com>
2017-04-29 15:29:13 +0900

    initial

---
+++ gps.py
@@ -0,0 +1,56 @@
+
+
+import serial
+import csv
+import pynmea2
+
+
+class Gps():
+
+    def __init__(self):
+        self.latitude = ''
+        self.longitude = ''
+        self.speed = ''
+        self.altitude = ''
+        self.course = ''
+        self.ser = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=0)
+
+    def create_file(self, filename="gps.csv"):
+        with open(filename, 'a') as csvfile:
+            csvwriter = csv.writer(csvfile, dialect='excel')
+            csvwriter.writerow(['latitude',
+                                'longitude',
+                                'speed',
+                                'altitude',
+                                'course'])
+
+    def write_date(self, filename="gps.csv"):
+        with open(filename, 'a') as csvfile:
+            csvwriter = csv.writer(csvfile, dialect='excel')
+            csvwriter.writerow(self.return_results())
+
+    def return_results(self):
+        return [self.latitude,
+                self.longitude,
+                self.speed,
+                self.altitude,
+                self.course]
+
+    def read_data(self):
+        while self.ser.in_waiting > 0:
+            msg = pynmea2.parse(ser.readline())
+
+            if msg.sentence_type = 'GGA':
+                self.latitude = msg.latitude
+                self.longitude = msg.longitude
+                self.altitude = msg.altitude
+
+            if msg.sentence_type = 'RMC':
+                self.speed = msg.spd_over_grnd
+                self.course = msg.true_course
+
+if __name__ == "__main__":
+    mygps = Gps()
+    mygps.create_file()
+    mygps.read_data()
+    mygps.write_data()
