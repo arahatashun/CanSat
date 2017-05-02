92a20f127be7ac26afa17432e2d3d671dcefbd1a
arahatashun <arahatashun@gmail.com>
2017-05-02 22:15:55 +0900

    indent

--- motor_test.cpp
+++ motor_test.cpp
@@ -6,6 +6,7 @@
 int main()
 {
 	MotorSoftPwm test1(1,70,70,10);
+
 	test1.motorMove();
     test1.setpwm(2,70,70,10);
     test1.motorMove();
@@ -17,8 +18,3 @@ int main()
 	test1.motorMove();
 	return 0;
 }
-
-	
-	
-	
-	
