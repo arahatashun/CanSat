90825abdab658b538342553817b2786f34d11061
Keidai Iiyama <keidaiiiyama@MacBook-Pro-5.local>
2017-05-02 15:30:37 +0900

    add new file

--- motor_softpwm.hpp
+++ motor_softpwm.hpp
@@ -8,17 +8,6 @@
 #include<wiringPi.h>
 #include<softPwm.h>
 
-const int RIGHTMOTOR1 17    //GPIO17
-const int RIGHTMOTOR2 27    //GPIO27
-const int LEFTMOTOR1 23    //GPIO23
-const int LEFTMOTOR2 24    //GPIO24
-const int RANGE 100
-const int STOP 0
-const int FWRD 1
-const int BACK 2
-const int RIGHT 3
-const int LEFT 4
-
 class MotorSoftPwm {
 	int mode, pwmvalue_r, pwmvalue_l, pwmsecond;
 public:
