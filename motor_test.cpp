1565d03e06ff5d060e45009f48d5b67e00847384
arahatashun <arahatashun@gmail.com>
2017-05-02 22:21:30 +0900

    indent

--- motor_test.cpp
+++ motor_test.cpp
@@ -3,14 +3,12 @@
 #include<cstdio>
 #include"motor_softpwm.hpp"
 
-int main()
-{
+int main(){
 	MotorSoftPwm test1(1,70,70,10);
-
 	test1.motorMove();
-  test1.setpwm(2,70,70,10);
-  test1.motorMove();
-  test1.setpwm(0,70,70,10);
+	test1.setpwm(2,70,70,10);
+	test1.motorMove();
+	test1.setpwm(0,70,70,10);
 	test1.motorMove();
 	test1.setpwm(3,70,70,10);
 	test1.motorMove();
