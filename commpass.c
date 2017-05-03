322d2fbdf56f91c62942654c177dc9ca1b60d980
arahatashun <arahatashun@gmail.com>
2017-05-03 22:54:20 +0900

    motor add

---
+++ commpass.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <math.h>
+
+#include <wiringPi.h>
+#include <wiringPiI2C.h>
+
+static const int　devid = 0x1e; //I2C address
+static const int mode_reg = 0x02;
+static const int mode_continuous = 0x00;
+static const int mode_single = 0x01;
+static const int x_msb_reg = 0x03;
+static const int x_lsb_reg = 0x04;
+static const int z_msb_reg = 0x05;
+static const int z_lsb_reg = 0x06;
+static const int y_msb_reg = 0x07;
+static const int y_lsb_reg = 0x08;
+static const double pi = 3.14159265;
+
+short read_out(int file,int msb_reg, int lsb_reg)
+{
+	uint8_t msb = wiringPiI2CReadReg8(file, msb_reg);
+	uint8_t lsb = wiringPiI2CReadReg8(file, lsb_reg);
+	short i = msb << 8| lsb;
+
+	return i;
+}
+
+int main()
+{
+/* WHO AM I */
+	int fd = wiringPiI2CSetup(devid);
+/* start senser */
+	if((wiringPiI2CWriteReg8(fd,mode_reg,mode_continuous))<0) {
+		printf("write error register mode_reg");
+	}
+	printf("write register:mode_reg")
+
+/* read X_MSB */
+	short x = read_out(fd, x_msb_reg, x_lsb_reg);
+	short y = read_out(fd, y_msb_reg, y_lsb_reg);
+	short z = read_out(fd, z_msb_reg, z_lsb_reg);
+//arctan(x/y)*pi/180
+	double angle=atan2((double) x,(double) y)*(180/pi);
+
+	printf("x:%d,y:%d,z:%d -angle %f¥n",x,y,z,angle);
+	return 0;
+}
---
+++ motor_run.c
@@ -0,0 +1,103 @@
+#include <wiringPi.h>
+#include <softPwm.h>
+
+static const int RIGHTMOTOR1　=　17;    //GPIO17
+static const int RIGHTMOTOR2　=　27;    //GPIO27
+static const int LEFTMOTOR1　=　23;    //GPIO23
+static const int LEFTMOTOR2　=　24;    //GPIO24
+static const int PWM_RANGE　=　100;
+static const int INITIAL_PWM_VAL = 0;
+static const int ZERO_PWM_VAL = 0;
+
+int pwm_initializer(){
+	//wiring Pi initialize
+	if(wiringPiSetupGpio() != 0) {
+		printf("setup faied");
+	}
+	//soft pwm initialize
+	softPwmCreate(RIGHTMOTOR1,INITIAL_PWM_VAL,PWM_RANGE);
+	softPwmCreate(RIGHTMOTOR2,INITIAL_PWM_VAL,PWM_RANGE);
+	softPwmCreate(LEFTMOTOR1,INITIAL_PWM_VAL,PWM_RANGE);
+	softPwmCreate(LEFTMOTOR2,INITIAL_PWM_VAL,PWM_RANGE);
+	return 0;
+}
+
+int motor_stop(int seconds)
+{
+	softPwmWrite(RIGHTMOTOR1,ZERO_PWM_VAL);
+	softPwmWrite(RIGHTMOTOR2,ZERO_PWM_VAL);
+	softPwmWrite(LEFTMOTOR1,ZERO_PWM_VAL);
+	softPwmWrite(LEFTMOTOR2,ZERO_PWM_VAL);
+	milliseconds=seconds*1000;
+	delay(milliseconds);
+	return 0;
+}
+
+int motor_cease()
+{
+	softPwmWrite(RIGHTMOTOR1,ZERO_PWM_VAL);
+	softPwmWrite(RIGHTMOTOR2,ZERO_PWM_VAL);
+	softPwmWrite(LEFTMOTOR1,ZERO_PWM_VAL);
+	softPwmWrite(LEFTMOTOR2,ZERO_PWM_VAL);
+	return 0;
+}
+
+int motor_forward(int seconds,int pwm_value)
+{
+	softPwmWrite(RIGHTMOTOR1,pwm_value);
+	softPwmWrite(RIGHTMOTOR2,ZERO_PWM_VAL);
+	softPwmWrite(LEFTMOTOR1,pwm_value);
+	softPwmWrite(LEFTMOTOR2,ZERO_PWM_VAL);
+	milliseconds=seconds*1000;
+	delay(milliseconds);
+	motor_cease();
+	return 0;
+}
+
+int motor_back(int seconds,int pwm_value)
+{
+	softPwmWrite(RIGHTMOTOR1,ZERO_PWM_VAL);
+	softPwmWrite(RIGHTMOTOR2,pwm_value);
+	softPwmWrite(LEFTMOTOR1,ZERO_PWM_VAL);
+	softPwmWrite(LEFTMOTOR2,pwm_value);
+	milliseconds=seconds*1000;
+	delay(milliseconds);
+	motor_cease();
+	return 0;
+}
+
+int motor_right(int seconds,int pwm_value)
+{
+	softPwmWrite(RIGHTMOTOR1,ZERO_PWM_VAL);
+	softPwmWrite(RIGHTMOTOR2,ZERO_PWM_VAL);
+	softPwmWrite(LEFTMOTOR1,pwm_value);
+	softPwmWrite(LEFTMOTOR2,ZERO_PWM_VAL);
+	milliseconds=seconds*1000;
+	delay(milliseconds);
+	motor_cease();
+	return 0;
+}
+
+int motor_left(int seconds,int pwm_value)
+{
+	softPwmWrite(RIGHTMOTOR1,pwm_value);
+	softPwmWrite(RIGHTMOTOR2,ZERO_PWM_VAL);
+	softPwmWrite(LEFTMOTOR1,ZERO_PWM_VAL);
+	softPwmWrite(LEFTMOTOR2,ZERO_PWM_VAL);
+	milliseconds=seconds*1000;
+	delay(milliseconds);
+	motor_cease();
+	return 0;
+}
+
+int main()
+{
+	pwm_initializer();
+	motor_forward(10,40);
+	motor_right(4,40);
+	motor_stop(10);
+	motor_left(4,40);
+	motor_back(10);
+	motor_forward(5);
+	return 0;
+}
