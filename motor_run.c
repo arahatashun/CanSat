1322fff5b85be754266f6b5899126c6e88f76978
arahatashun <arahatashun@gmail.com>
2017-05-04 13:14:15 +0900

    debug

--- motor_run.c
+++ motor_run.c
@@ -12,9 +12,11 @@ static const int PWM_RANGE = 100;
 static const int INITIAL_PWM_VAL = 0;
 static const int ZERO_PWM_VAL = 0;
 
-int pwm_initializer(){
+int pwm_initializer()
+{
 	//wiring Pi initialize
-	if(wiringPiSetupGpio() != 0) {
+	if(wiringPiSetupGpio() != 0)
+	{
 		printf("setup faied");
 	}
 	//soft pwm initialize
@@ -72,7 +74,7 @@ int motor_back(int seconds,int pwm_value)
 int motor_right(int seconds,int pwm_value)
 {
 	softPwmWrite(RIGHTMOTOR1,ZERO_PWM_VAL);
-	softPwmWrite(RIGHTMOTOR2,ZERO_PWM_VAL);
+	softPwmWrite(RIGHTMOTOR2,pwm_value);
 	softPwmWrite(LEFTMOTOR1,pwm_value);
 	softPwmWrite(LEFTMOTOR2,ZERO_PWM_VAL);
 	int milliseconds=seconds*1000;
@@ -86,14 +88,15 @@ int motor_left(int seconds,int pwm_value)
 	softPwmWrite(RIGHTMOTOR1,pwm_value);
 	softPwmWrite(RIGHTMOTOR2,ZERO_PWM_VAL);
 	softPwmWrite(LEFTMOTOR1,ZERO_PWM_VAL);
-	softPwmWrite(LEFTMOTOR2,ZERO_PWM_VAL);
+	softPwmWrite(LEFTMOTOR2,pwm_value);
 	int milliseconds=seconds*1000;
 	delay(milliseconds);
 	motor_cease();
 	return 0;
 }
 
-void handler(int signum) {
+void handler(int signum)
+{
 	motor_cease();
 }
 
@@ -103,11 +106,11 @@ int main()
 {
 	signal(SIGINT, handler);
 	pwm_initializer();
-	motor_forward(10,40);
+	motor_forward(10,70);
 	motor_right(4,40);
-	motor_stop(10);
+	motor_stop(5);
 	motor_left(4,40);
 	motor_back(10,40);
-	motor_forward(5,40);
+	motor_forward(5,80);
 	return 0;
 }
