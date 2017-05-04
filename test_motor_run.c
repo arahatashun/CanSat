98496415f9f374e014eda82eef0c596cb9cdb50d
arahatashun <arahatashun@gmail.com>
2017-05-04 16:36:14 +0900

    debug

--- test_motor_run.c
+++ test_motor_run.c
@@ -4,11 +4,11 @@ int main()
 {
 	pwm_initializer();
 	motor_forward(100,70);
-	motor_right(400,40);
-	motor_stop(500);
-	motor_left(400,40);
+	motor_right(100,40);
+	motor_stop(100);
+	motor_left(100,40);
 	motor_back(100,40);
-	motor_forward(500,80);
+	motor_forward(100,80);
 	motor_stop(5);
 	return 0;
 }
