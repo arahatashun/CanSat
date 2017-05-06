db33965cfa4bf1da81aa89b60cbf82826a3777c6
arahatashun <arahatashun@gmail.com>
2017-05-06 22:31:03 +0900

    header

--- mitibiki.c
+++ mitibiki.c
@@ -1,25 +1,16 @@
 #include <stdio.h>
 #include <math.h>
-#include <gps.h>
 
 //note: seikei toukei ni izon
 static const double target_latitude = 35.7133352;//ido
 static const double target_longitude = 139.7599809;//keido
-static const double pi = 3.14159265;
+static const double PI = 3.14159265;
 
 double target_angle(double lat,double lon)
 {
   double lat_offset = target_latitude - lat;
   double lon_offset = target_longitude - lon;
-  double angle = atan2(-lon_offset,-lat_offset)*(180/pi) + 180;
+  double angle = atan2(-lon_offset,-lat_offset)*(180/PI) + 180;
+  printf("target_angle : %f\n",angle);
   return angle;
-}
-
-int main()
-{
-  gps_init();
-  loc_t data;
-  gps_location(&data);
-  double angle = target_angle(data.latitude,data.longitude);
-  printf("%f",angle);
 }
