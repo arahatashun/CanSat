9a41e08a2e5c97beb42c400008a0853e93dc81aa
arahatashun <arahatashun@gmail.com>
2017-05-06 23:23:55 +0900

    debug

--- test.c
+++ test.c
@@ -4,7 +4,7 @@
 #include "mitibiki.h"
 
 double course;//current angle
-double target_angle;
+double angle_to_go;
 double delta_angle;//the difference between current angle and angle to go
 
 int sensor_initializer()
@@ -17,15 +17,31 @@ int sensor_initializer()
 
 int update_angle()
 {
-  course = get_angle();
-  target_angle = target_angle();
-
-  return 0;
+  course = get_angle();//compass angle
+  angle_to_go = target_angle();
+  delta_angle = course - angle_to_go;
+  return delta_angle;
 }
 
 int decide_route()
 {
-
+  while(0<=update_angle() && update_angle()<=180)
+  {
+    motor_left(50,40);
+  }
+  while(180<update_angle() && update_angle()<=360)
+  {
+    motor_right(50,40);
+  }
+  while(-180<update_angle() && update_angle()<0)
+  {
+    motor_right(50,40);
+  }
+  while(-360<=update_angle() && update_angle()<=-180)
+  {
+    motor_left(50,40);
+  }
+  return 0;
 }
 
 int main()
@@ -33,6 +49,7 @@ int main()
   sensor_initializer();
   while (1)
   {
-
+    decide_route();
   }
+  return 0;
 }
