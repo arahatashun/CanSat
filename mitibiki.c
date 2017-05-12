8a45d70649690940569f9e040c5b42c0e188d701
arahatashun <arahatashun@gmail.com>
2017-05-12 18:40:19 +0900

    debug

--- mitibiki.c
+++ mitibiki.c
@@ -7,7 +7,7 @@ static const double target_longitude = 139.7599809;//keido
 static const double PI = 3.14159265;
 static const double EARTH_RADIUS = 6378137;
 //構造体で角度と距離返す方がいいかもしれないs
-static loc_t data;
+loc_t data;
 
 typedef struct cartesian_coordinates{
   double x;
