ae6b40970d8845040fdf04d34b774fe872b52081
arahatashun <arahatashun@gmail.com>
2017-05-16 09:01:49 +0900

    motor.cの変更に伴う

--- motor_test.c
+++ motor_test.c
@@ -1,10 +1,12 @@
-#include "motor.h"
+
 #include <signal.h>
 #include <stdlib.h>
+#include <wiringPi.h>
+#include "motor.h"
 
 void handler(int signum)
 {
-  motor_stop(10);
+  motor_stop();
   exit(1);
 }
 
@@ -13,7 +15,10 @@ int main()
 {
   signal(SIGINT, handler);
   pwm_initializer();
-  motor_forward(10000, 60);
-  motor_left(500,50);
-  motor_right(500,50);
+  motor_forward(60);
+  delay(50000)
+  motor_left(50);
+  delay(50000)
+  motor_right(50);
+  delay(50000)
 }
