e5e52c22d6a356cf33828f46e1b623db2af8f583
kdricemt <keidaiiiyama@MacBook-Pro-5.local>
2017-05-22 08:24:26 +0900

    edit acclgyro

--- acclgyro.h
+++ acclgyro.h
@@ -4,15 +4,15 @@
 #define ACCLGYRO_H
 
 //構造体宣言(typedef)
-typedef struct acclgyro{
-    double acclX_scaled;//the values of accleration
-    double acclY_scaled;
-    double acclZ_scaled;
-    double gyroX_scaled;
-    double gyroY_scaled;
-    double gyroZ_scaled;//the values of gyroscope
-    double x_rotation;
-    double y_rotation;//XY-axis rotation
+typedef struct acclgyro {
+  double acclX_scaled;//the values of accleration
+  double acclY_scaled;
+  double acclZ_scaled;
+  double gyroX_scaled;
+  double gyroY_scaled;
+  double gyroZ_scaled;//the values of gyroscope
+  double x_rotation;
+  double y_rotation;//XY-axis rotation
 } Acclgyro;
 
 int acclgyro_initializer();   //initialization
--- acclgyro_test.c
+++ acclgyro_test.c
@@ -3,15 +3,15 @@
 #include <stdio.h>
 #include <math.h>
 #include <wiringPiI2C.h>
-#include "acclgyro.h"
+#include "acclgyro.h" #includea
 
-int main()
-{
-	Acclgyro acclgyro_data;    //オブジェクト生成
+int main(){
+	Acclgyro acclgyro_data; //オブジェクト生成
 	acclgyro_initializer();
-	while(1) {
+	while(1)
+	{
 		print_acclgyro(&acclgyro_data);
-    is_reverse(&acclgyro_data);
+		is_reverse(&acclgyro_data);
 		delay(1000);
 	}
 	return 0;
