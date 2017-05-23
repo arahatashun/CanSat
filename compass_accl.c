4df371c2673850c2248e6814689ca95e507f6418
cagalliorb <hk912united7@gmail.com>
2017-05-23 17:23:35 +0900

    thetaの追加

--- compass_accl.c
+++ compass_accl.c
@@ -3,6 +3,8 @@
 #include <math.h>
 #include <stdio.h>
 
+static const double PI = 3.14159265359;
+
 int main()
 {
   	acclgyro_initializer();
@@ -13,9 +15,12 @@ int main()
     double xcompass_knd = 0;
     double ycompass_knd = 0;
     double zcompass_knd = 0;
-    double theta = 0;
-    double phi = 0;
-
+    double phi_radian = 0;
+    double psi_radian = 0;
+    double theta_radian = 0;
+    double phi_degree = 0;
+    double psi_degree = 0;
+    double theta_degree = 0;
     while(1)
     {
       acclx_knd = (double)get_acclx();
@@ -30,9 +35,13 @@ int main()
       printf("compassx = %lf", xcompass_knd);
       printf("compassy = %lf", ycompass_knd);
       printf("compassz = %lf", zcompass_knd);
-      theta = atan2(accly_knd, acclz_knd);
-      phi = atan2(-acclx_knd, accly_knd*sin(theta) + acclz_knd*cos(theta));
-      printf("theta = %lf", theta);
-      printf("phi = %lf", phi);
+      phi_radian = atan2(accly_knd, acclz_knd);
+      psi_radian = atan2(-acclx_knd, accly_knd*sin(theta) + acclz_knd*cos(theta));
+      phi_degree = atan2(accly_knd, acclz_knd)*180.0/PI;
+      psi_degree = atan2(-acclx_knd, accly_knd*sin(theta) + acclz_knd*cos(theta))*180.0/PI;
+      printf("theta = %lf", phi_degree);
+      printf("phi = %lf", psi_degree);
+      theta = atan2((acclz_knd*sin(phi_radian) - accly_knd*cos(phi_radian)) / (acclx_knd*cos(psi_radian) + accly_knd*sin(psi_radian)*sin(phi_radian) + acclz_knd*sin(psi_radian)*cos(phi_radian))) * 180.0/PI + 180.0);
+      printf("phi = %lf", psi_degree);
     }
 }
