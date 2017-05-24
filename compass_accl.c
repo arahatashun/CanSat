4d1fa190562379d06182c5668ecb41c86931c5f4
cagalliorb <hk912united7@gmail.com>
2017-05-24 11:21:39 +0900

    int

--- compass_accl.c
+++ compass_accl.c
@@ -21,12 +21,12 @@ int main()
     double psi_radian = 0;
     double phi_degree = 0;
     double psi_degree = 0;
-    double atan_y_1 = 0;
-    double atan_y_2 = 0;
-    double atan_y_3 = 0;
-    double atan_y_4 = 0;
-    double atan_y_5 = 0;
-    double atan_y_6 = 0;
+    int atan_y_1 = 0;
+    int atan_y_2 = 0;
+    int atan_y_3 = 0;
+    int atan_y_4 = 0;
+    int atan_y_5 = 0;
+    int atan_y_6 = 0;
     double atan_x = 0;
     double theta_degree = 0;
     while(1)
@@ -51,9 +51,9 @@ int main()
       printf("psi = %f\n", psi_degree);
       printf("sin(phi) = %f\n", sin(phi_radian));
       printf("cos(phi) = %f\n", cos(phi_radian));
-      atan_y_1 =  100000*acclz_knd;
+      atan_y_1 = (int) 100000*acclz_knd;
       printf("100000 * %f = %f\n", acclz_knd, atan_y_1);
-      atan_y_2 = 100000*sin(phi_radian);
+      atan_y_2 = (int) 100000*sin(phi_radian);
       printf("100000 * %f = %f\n", sin(phi_radian), atan_y_2);
       atan_y_3 = atan_y_1 * atan_y_2;
       printf("atan_y_1 * atan_y_2 = %f\n", atan_y_3);
@@ -61,12 +61,12 @@ int main()
       atan_x = acclx_knd*cos(psi_radian) + accly_knd*sin(psi_radian)*sin(phi_radian) + acclz_knd*sin(psi_radian)*cos(phi_radian);
       printf("atan_y = %f", atan_y);
       printf("atan_x = %f", atan_x);*/
-      atan_y_4 = 100000*accly_knd;
+      atan_y_4 =(int) 100000*accly_knd;
       printf("100000 * %f = %f\n", accly_knd, atan_y_4);
-      atan_y_5 = 100000*cos(phi_radian);
+      atan_y_5 =(int) 100000*cos(phi_radian);
       printf("100000 * %f = %f\n", cos(phi_radian), atan_y_5);
       atan_y_6 = atan_y_4 * atan_y_5;
-      printf("atan_y_1 * atan_y_2 = %f\n", atan_y_6);
+      printf("atan_y_4 * atan_y_5 = %f\n", atan_y_6);
       /*theta_degree = atan2(atan_y_1, atan_x)*(180.0/PI)+ 180.0;
       printf("theta = %f\n", theta_degree);*/
       delay(1000);
