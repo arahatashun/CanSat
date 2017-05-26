fa1aaafa7dbcc3bf623273e7a1dd111233a01d93
arahatashun <arahatashun@gmail.com>
2017-05-26 14:59:10 +0900

    debug

--- mitibiki.h
+++ mitibiki.h
@@ -2,7 +2,6 @@
 #define MITIBIKI_H
 
 double calc_target_angle(double lat,double lon);
-double dist_on_sphere(double target_latitude,double target_longitude,
-                              double current_lat, double current_lon);
+double dist_on_sphere(double current_lat, double current_lon);
 
 #endif
