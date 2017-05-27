18a1596024880f04ac9b669348ea26dd778ca79a
arahatashun <arahatashun@gmail.com>
2017-05-27 13:54:30 +0900

    debug

--- mitibiki.c
+++ mitibiki.c
@@ -3,8 +3,8 @@
 #include <gps.h>
 #include "mitibiki.h"
 //note: seikei toukei ni izon
-static const double target_latitude = 35.717705;//緯度
-static const double target_longitude = 139.759385;//経度
+static const double target_latitude = 35.713813;//緯度
+static const double target_longitude = 139.760066;//経度
 static const double PI = 3.14159265;
 static const double EARTH_RADIUS = 6378137;
 
