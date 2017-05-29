120c671afc299385424d8928af46f5edbfde6dd6
arahatashun <arahatashun@gmail.com>
2017-05-30 01:05:03 +0900

    debug

--- pid.c
+++ pid.c
@@ -21,27 +21,28 @@ int pid_limiter(pid* pid_limits)
 int compute_output(pid* pid_comp)
 {
   unsigned int now = millis();
-  error = pid_comp->setpoint - pid_comp->input;//目標値ー入力値:偏差
+  double error = pid_comp->setpoint - pid_comp->input;//目標値ー入力値:偏差
 
   if(pid_comp->lastTime==0)//初回
   {
     int deltaT = 0;
     pid_comp->differential = 0;
+    pid_comp->integral += error * deltaT;
   }
   else
   {
     int deltaT = now -pid_comp->lastTime;
     pid_comp->differential = (error-pid_comp->prev_error)/deltaT;
+    pid_comp->integral += error * deltaT;
   }
 
-  pid_comp->integral += error * deltaT;
   pid_comp->prev_error = error;//prev_errorを更新
   pid_comp->lastTime = now;//最後の時刻を更新
   pid_comp->output = pid_comp->Kp * error +//propotional
                       pid_comp->Ki * pid_comp->integral+//integral
                         pid_comp->Kd * pid_comp->differential;//differential
 
-  pid_limiter(*pid_comp);
+  pid_limiter(&pid_comp);
   return 0;
 }
 
