dac223c57f4518ab9f07c1fbf8605ae97da910b5
cagalliorb <hk912united7@gmail.com>
2017-05-29 17:55:27 +0900

    debug

--- compass.c
+++ compass.c
@@ -54,7 +54,7 @@ static short read_out(int file,int msb_reg, int lsb_reg)
 int compass_read(Cmps *compass_data)
 {
 	//WriteReg8
-	WPI2CWReg8 = wiringPiI2CWriteReg8(fd,mode_reg,mode_single);
+	WPI2CWReg8 = wiringPiI2CWriteReg8(fd,mode_reg,mode_continuous);
 	if(WPI2CWReg8 == -1)
 	{
 		printf("Compass write error register mode_reg\n");
