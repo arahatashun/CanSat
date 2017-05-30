33edd6c6ed2429c8cc39ef6a83f239a1933b212e
cagalliorb <hk912united7@gmail.com>
2017-05-30 21:38:23 +0900

    debug

--- luxsensor_test.c
+++ luxsensor_test.c
@@ -4,12 +4,13 @@
 
 int main()
 {
-	luxsensor_initializer();
 	int lux_value = 0;
 	while(1)
 	{
+		luxsensor_initializer();
 		lux_value = getLux();
 		printf("lux :%d", lux_value);
 		delay(1000);
+		wiringPiI2CWriteReg8(fd, TSL2561_COMMAND_BIT, TSL2561_CONTROL_POWEROFF);
 	}
 }
