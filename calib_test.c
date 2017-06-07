55a25185b207b5e42bee1874806614efde40053f
cagalliorb <hk912united7@gmail.com>
2017-06-07 17:30:25 +0900

    debug

--- calib_test.c
+++ calib_test.c
@@ -12,13 +12,15 @@ int main()
 	delay(2000);
 	double compass_x = 0;
 	double compass_y = 0;
-	double compass_angle = 0;
+	double compass_deviated_angle = 0;
+	double compass_true_angle = 0;
 	while(1)
 	{
 		compass_read(&compass_data);
 		compass_x = compass_data.compassx_value - compass_offset.compassx_offset;
 		compass_y = compass_data.compassy_value - compass_offset.compassy_offset;
-		compass_angle = calc_compass_angle(compass_x, compass_y);
+		compass_deviated_angle = calc_compass_angle(compass_x, compass_y);
+		compass_true_angle = cal_deviated_angle(compass_deviated_angle);
 		printf("compass_angle=%f\n",compass_angle);
 		delay(1000);
 	}
