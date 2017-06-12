311f4ad89d60e8760b2c615c40a325781c2f5e98
arahatashun <arahatashun@gmail.com>
2017-06-12 09:36:30 +0900

    gps

---
+++ gps_src/serial.h
@@ -0,0 +1,17 @@
+#ifndef _SERIAL_H_
+#define _SERIAL_H_
+
+#include <inttypes.h>
+
+#ifndef PORTNAME
+#define PORTNAME "/dev/ttyS0"//for zero and 3
+//for 2 ttyAMA0
+#endif
+
+void serial_init(void);
+void serial_config(void);
+void serial_println(const char *, int);
+int serial_readln(char *, int);
+void serial_close(void);
+
+#endif
