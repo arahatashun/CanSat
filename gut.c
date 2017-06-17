22ac6a3f82345d151948758bb893b39a6bab9031
cagalliorb <hk912united7@gmail.com>
2017-06-17 19:18:03 +0900

    gut_test

--- gut.c
+++ gut.c
@@ -1,29 +1,35 @@
-//gut.c
+#include <stdio.h>
+#include <wiringPi.h>
+#include "gut.h"
 
-#include<stdio.h>
-#include<wiringPi.h>
-#include"gut.h"
-
-static const int gut_pin = 0;
-static const int electric_on_time = 4000; //ms,4秒間
+static const int gut_pin = 10;
+static const int electric_on_time = 4000; //ms・4秒間
 
 int cut_initializer(){
-    if(wiringPiSetup() == -1){
-         printf("error wiringPi setup\n");
-         return -1;
-        }
-    pinMode(gut_pin, OUTPUT);
-    return 0;
+
+	if(wiringPiSetupGpio() == -1)
+	{
+		printf("error wiringPi setup\n");
+		return -1;
+	}
+	else
+	{
+		printf("wiringPisetup success\n");
+	}
+	pinMode(gut_pin, OUTPUT);
+	return 0;
 }
 
 int cut(){
-    int i=0;
-    for(i=0;i<2;i++){
-      //念のため二回流す
-      digitalWrite(gut_pin,1);
-      delay(electric_on_time);
-      digitalWrite(gut_pin,0);
-      delay(10);
-    }
-    return 0;
+	int i=0;
+	printf("cut now\n");
+	for(i=0; i<2; i++) {
+		//念のため二回流す
+		printf("%d\n", i);
+		digitalWrite(gut_pin,1);
+		delay(electric_on_time);
+		digitalWrite(gut_pin,0);
+		delay(10);
+	}
+	return 0;
 }
