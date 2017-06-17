4903f116b4225a43f652f6df7e030049eafa51de
arahatashun <arahatashun@gmail.com>
2017-06-17 19:52:37 +0900

    関数の分割

--- object_recognition/parachute.cpp
+++ object_recognition/parachute.cpp
@@ -5,8 +5,10 @@
 #pragma comment(lib,"opencv_world320.lib")
 
 static const int N = 256;//文字列の長さ
-int main(void) {
-	//TODO 関数の分割
+
+//写真をとってそのpathを返す
+char* takePhoto(void)
+{
 	char full_command[N];
 	char front_command[] = "sudo raspistill -o ";//command
 	char full_path[N];
@@ -21,18 +23,21 @@ int main(void) {
 	sprintf(full_path, "%s%s%s",directry_path, name_path, file_extention);
 	sprintf(full_command, "%s%s", front_command, full_path);//コマンドの文字列をつなげる。
 	system(full_command);//raspistillで静止画を撮って日時を含むファイル名で保存。
+	return full_path;
+}
 
-	cv::Mat src,dst,dst_filtered;
-	dst_filtered = cv::Scalar(0, 0, 0);//画像の初期化
+int main(void) {
+	cv::Mat src,hsv,hsv_filtered;/
+	hsv_filtered = cv::Scalar(0, 0, 0);//画像の初期化
 	double count = 0; //赤色を認識したピクセルの数
 	double percentage = 0; //割合
-	src =cv::imread(full_path);
-	cv::cvtColor(src, dst, CV_BGR2HSV);//入力画像(src)をhsv色空間(dst)に変換
+	src =cv::imread(takePhoto());
+	cv::cvtColor(src, hsv, CV_BGR2HSV);//入力画像(src)をhsv色空間(hsv)に変換
 	//inRange(入力画像,下界画像,上界画像,出力画像)
 	//「HSV」は、色を色相(Hue)・彩度(Saturation)・明度(Value)
-	cv::inRange(dst, cv::Scalar(160, 150, 0), cv::Scalar(190, 255, 255), dst_filtered);
-	count = cv::countNonZero(dst_filtered);//赤色部分の面積を計算
-	double Area = dst.rows*dst.cols;//全ピクセル数の計算
+	cv::inRange(hsv, cv::Scalar(160, 150, 0), cv::Scalar(190, 255, 255), hsv_filtered);
+	count = cv::countNonZero(hsv_filtered);//赤色部分の面積を計算
+	double Area = hsv.rows*hsv.cols;//全ピクセル数の計算
 	percentage = (count / Area)*100;//百分率で計算
 	printf("赤色の面積の割合は%f\n", percentage);
 }
