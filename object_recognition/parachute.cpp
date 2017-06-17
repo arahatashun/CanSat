f09cc9aa333dda3852d3c8cf30e8f433615d6d37
arahatashun <arahatashun@gmail.com>
2017-06-17 20:38:45 +0900

    debug

--- object_recognition/parachute.cpp
+++ object_recognition/parachute.cpp
@@ -12,7 +12,7 @@ char* takePhoto(void)
 	char full_command[N];
 	char front_command[] = "sudo raspistill -o ";//command
 	static char full_path[N];//NOTE 自動変数をreturn するために使った. smartなやり方か?
-	char directry_path[] = "/home/pi/object";//pathの先頭
+	char directry_path[] = "/home/pi/Pictures";//pathの先頭
 	char name_path[N];//時間を文字列に変換するときに代入する変数
 	char file_extention[] = ".jpg";//拡張子
 	time_t timer;//時刻を受け取る変数
@@ -23,6 +23,7 @@ char* takePhoto(void)
 	sprintf(full_path, "%s%s%s",directry_path, name_path, file_extention);
 	sprintf(full_command, "%s%s", front_command, full_path);//コマンドの文字列をつなげる。
 	system(full_command);//raspistillで静止画を撮って日時を含むファイル名で保存。
+	printf("%s\n",full_command);
 	//NOTE system関数以外を使うべきか?
 	return full_path;
 }
