caba15f53d11d02ad0f9a30f2891736c44e011f8
arahatashun <arahatashun@gmail.com>
2017-06-17 18:58:10 +0900

    debug

--- object_recognition/parachute.cpp
+++ object_recognition/parachute.cpp
@@ -25,13 +25,13 @@ int main(void) {
 	dst_filtered = cv::Scalar(0, 0, 0);//画像の初期化
 	double count = 0; //赤色を認識したピクセルの数
 	double percentage = 0; //割合
-	double Area = dst.rows*dst.cols;//全ピクセル数
 	src =cv::imread(full_path);
 	cv::cvtColor(src, dst, CV_BGR2HSV);//入力画像(src)をhsv色空間(dst)に変換
 	//inRange(入力画像,下界画像,上界画像,出力画像)
 	//「HSV」は、色を色相(Hue)・彩度(Saturation)・明度(Value)
 	cv::inRange(dst, cv::Scalar(160, 150, 0), cv::Scalar(190, 255, 255), dst_filtered);
 	count = cv::countNonZero(dst_filtered);//赤色部分の面積を計算
+	double Area = dst.rows*dst.cols;//全ピクセル数の計算
 	percentage = count / Area;//割合を計算
 	printf("赤色の面積の割合は%f\n", percentage);
 }
