8abacf4650f89d25f8d0ef74953d468131708bc8
arahatashun <arahatashun@gmail.com>
2017-06-19 22:18:07 +0900

    debug

--- object_recognition/goal.cpp
+++ object_recognition/goal.cpp
@@ -2,12 +2,36 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include "camera.hpp"
+#include "../motor.h"
 
 #pragma comment(lib,"opencv_world320.lib")
 
+static const int ROTATE_POWER = 20;
+static const int ROTATE_MILLISECONDS = 200;
+static const int LEFT_MAX = -200;
+static const int RIGHT_MAX = 200;
+static const int CENTER_THRESHOLD = 100;//-100~100で直進するようにする
 int main(void)
 {
 	cv::Mat red = Mred(takePhoto());
 	countArea(red);
-	getCenter(red);
+while(1)
+{
+	if(LEFT_MAX<getCenter(red)<-CENTER_THRESHOLD)
+	{
+		motor_right(ROTATE_POWER);
+		delay(ROTATE_MILLISECONDS);
+	}
+	if(CENTER_THRESHOLD<getCenter(red)<RIGHT_MAX)
+	{
+		motor_left(ROTATE_POWER);
+		delay(ROTATE_MILLISECONDS);
+	}
+	if(-CENTER_THRESHOLD<getCenter(red)<CENTER_THRESHOLD)
+	{
+		motor_forward(100);
+		delay(200);
+	}
+ }
+return 0;
 }
