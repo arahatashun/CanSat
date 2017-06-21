5224c1d9e969bc70f58cb121f02600e9b390743f
arahatashun <arahatashun@gmail.com>
2017-06-21 16:01:03 +0900

    debug

--- acclgyro.h
+++ acclgyro.h
@@ -1,24 +1,23 @@
 #ifndef ACCLGYRO_H
 #define ACCLGYRO_H
 
-//構造体宣言(typedef)
-typedef struct acclgyro {
+//三軸加速度(単位G)
+typedef struct accl {
   double acclX_scaled;//the values of accleration
   double acclY_scaled;
   double acclZ_scaled;
-  double gyroX_scaled;
-  double gyroY_scaled;
-  double gyroZ_scaled;//the values of gyroscope
-  double x_rotation;
-  double y_rotation;//XY-axis rotation
-} Acclgyro;
+}Accl;
 
-int acclgyro_initializer();   //initialization
-int accl_and_rotation_read(Acclgyro *acclgyro_data);
-int gyro_read(Acclgyro *acclgyro_data);
-int print_acclgyro(Acclgyro *acclgyro_data);    //六軸センサーの値を返す
-int is_reverse(Acclgyro *acclgyro_data);  //反転していたら1を返す
-int acclgyro_value_initialize(Acclgyro *acclgyro_init);
-double cal_roll(double,double);
-double cal_pitch(double,double,double,double);
+//角速度 degrees/second
+//NOTE z軸が上なので基本的にそれをみると良い
+typedef struct gyro {
+  double gyroX_scaled;//the values of accleration
+  double gyroY_scaled;
+  double gyroZ_scaled;
+}Gyro;
+
+int acclGyro_initialize(void);
+int readGyro(Gyro *data);
+int readAccl(Accl*data);
+int isReverse(void);
 #endif
---
+++ gyro_test.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+#include <wiringPiI2C.h>
+#include "acclgyro.h"
+
+int main(){
+	Accl accl_data;
+	Gyto gyro_data;
+	acclgyro_initialize();
+	int now = 0;
+	int lastTime = millis();
+	double delat_theta = 0;
+	while(1)
+	{
+		readGyro(&gyro_data);
+		now = millis();
+		int delta_time = now-lastTime;
+		delat_theta += gyro_data.gyroZ_scaled*delta_time/1000;//convert2seconds
+		printf("%f\n",delat_theta);
+		lastTime = now;
+	}
+
+	return 0;
+}
