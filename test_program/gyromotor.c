8397c53c6c1535a76d61bf570258bbe72894c554
arahatashun <arahatashun@gmail.com>
2017-06-24 17:14:42 +0900

    dbeug

--- test_program/gyromotor.c
+++ test_program/gyromotor.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include "../motor.h"
 #include "../acclgyro.h"
+#include "../pid.h"
 
 
 void handler(int signum)
@@ -17,6 +18,11 @@ void handler(int signum)
 
 int main()
 {
+	Pid* pid;
+	pid_initialize(pid);
+	pid -> Kp = 0.4;
+	pid -> Ki = 0.4;
+	pid -> setpoint = 0;
 	signal(SIGINT, handler);
 	Accl accl_data;
 	Gyro gyro_data;
@@ -33,9 +39,10 @@ int main()
 		delta_theta -= gyro_data.gyroZ_scaled*delta_time/1000;//convert2seconds
 		printf("%f\n",delta_theta);
 		lastTime = now;
-		double rotate_power =  90 - delta_theta;
-		printf("rotate power:%f\n",rotate_power);
-		motor_rotate(rotate_power);
+		pid -> input =  90 - delta_theta;
+		int pwm = compute_output(pid);
+		printf("rotate power:%f\n",pwm);
+		motor_rotate(pwm);
 	}
 	return 0;
 }
