a1de7f239d18a79e10e0559c3e75b3fbf491b347
cagalliorb <hk912united7@gmail.com>
2017-06-24 20:59:27 +0900

    debug

--- compass_accl.c
+++ compass_accl.c
@@ -11,17 +11,16 @@ int main()
 	double phi_rad = 0;
 	double psi_rad = 0;
 	double theta_degree = 0;
-	Accl acclgyro_data;
+	Accl accl_data;
 	Cmps compass_data;
 	acclGyro_initialize();
 	compass_initialize();
 	while(1)
 	{
-		readAccl(&acclgyro_data);
+		readAccl(&accl_data);
 		print_compass(&compass_data);
-		phi_rad = cal_roll(acclgyro_data.acclY_scaled, acclgyro_data.acclZ_scaled);
-		psi_rad = cal_pitch(acclgyro_data.acclX_scaled, acclgyro_data.acclY_scaled,
-		                    acclgyro_data.acclZ_scaled, phi_rad);
+		phi_rad = cal_roll(&accl_data);
+		psi_rad = cal_pitch(&accl_data);
 		printf("phi_degree = %f\n", phi_rad*180.0/PI);
 		printf("psi_degree = %f\n", psi_rad*180.0/PI);
 		double theta_degree = cal_deg_acclcompass(compass_data.x_value,compass_data.y_value,
