1620c4a283f63950b895c65d4c19cd978dd0a609
cagalliorb <hk912united7@gmail.com>
2017-06-25 20:27:38 +0900

    debug

--- mitibiki.c
+++ mitibiki.c
@@ -5,8 +5,8 @@
 //note: seikei toukei ni izon
 
 
-static const double target_latitude = 35.7179448;//緯度
-static const double target_longitude = 139.7603420;//経度
+static const double target_latitude = 35.7156859;//緯度
+static const double target_longitude = 139.7617485;//経度
 static const double PI = 3.14159265;
 static const double EARTH_RADIUS = 6378137;
 
