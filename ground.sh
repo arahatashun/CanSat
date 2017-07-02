859f37a30c0a67cb25d05dcab9d9ea7398b5d84f
arahatashun <arahatashun@gmail.com>
2017-07-02 23:32:49 +0900

    deub

--- ground.sh
+++ ground.sh
@@ -3,6 +3,6 @@
 trap 'echo SIGIN Handle;sudo ./sigintHandle.out' SIGINT
 echo "start ground run"
 echo "Executing, ground_compass.out"
-sudo stdbuf -o0 -e0 ./ground_compass.out | tee ~/Documents/`date +%Y%m%d_%H-%M-%S`.txt
+sudo stdbuf -o0 -e0 ./ground_compass.out | tee /home/pi/Documents/`date +%Y%m%d_%H-%M-%S`.txt
 echo "Executing, goal.out"
-sudo stdbuf -o0 -e0 ./object_recognition/goal.out | tee ~/Documents/`date +%Y%m%d_%H-%M-%S`.txt
+sudo stdbuf -o0 -e0 ./object_recognition/goal.out | tee /home/pi/Documents/`date +%Y%m%d_%H-%M-%S`.txt
