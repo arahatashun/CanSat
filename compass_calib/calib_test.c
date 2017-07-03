2813369588a8a980f0a3524fd5479dc13451575e
cagalliorb <hk912united7@gmail.com>
2017-07-03 09:51:25 +0900

    debug

--- compass_calib/calib_test.c
+++ compass_calib/calib_test.c
@@ -12,17 +12,14 @@ int main()
 	Cmps_offset compass_offset;
 	cal_maxmin_compass(&compass_offset,&compass_data);
 	delay(2000);
-	double compass_x = 0;
-	double compass_y = 0;
-	double compass_angle = 0;
 	printf("compassx_offset = %f\n", compass_offset.compassx_offset);
 	printf("compassy_offset = %f\n", compass_offset.compassy_offset);
 	while(1)
 	{
 		compass_read(&compass_data);
-		compass_x = compass_data.x_value - compass_offset.compassx_offset;
-		compass_y = compass_data.y_value - compass_offset.compassy_offset;
-		compass_angle = calc_compass_angle(compass_x, compass_y);
+		double compass_x = compass_data.x_value - compass_offset.compassx_offset;
+		double compass_y = compass_data.y_value - compass_offset.compassy_offset;
+		double compass_angle = calc_compass_angle(compass_x, compass_y);
 		printf("compass_angle=%f\n",compass_angle);
 		delay(1000);
 	}
