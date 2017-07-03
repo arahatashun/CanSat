23f2ab3f7f8662fccef03fba073b1893e9de33b3
arahatashun <arahatashun@gmail.com>
2017-07-03 21:58:09 +0900

    debug

--- test_program/gyro_test.c
+++ test_program/gyro_test.c
@@ -15,7 +15,7 @@ int main(){
 		readGyro(&gyro_data);
 		now = millis();
 		int delta_time = now-lastTime;
-		printf("gyro_data.gyroZ_scaled %d\n",gyro_data.gyroZ_scaled);
+		printf("gyro_data.gyroZ_scaled %f\n",gyro_data.gyroZ_scaled);
 		delat_theta += gyro_data.gyroZ_scaled*delta_time/1000;//convert2seconds
 		printf("%f\n",delat_theta);
 		lastTime = now;
