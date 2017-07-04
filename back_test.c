ee3b53248b77f4558ffc4bfcdc76cc79163ffd13
cagalliorb <hk912united7@gmail.com>
2017-07-04 15:57:47 +0900

    debug

--- back_test.c
+++ back_test.c
@@ -17,12 +17,14 @@ int main()
 	signal(SIGINT, handler);
 	pwm_initialize();
 	motor_forward(100);
-	delay(1000);
-	for(i=2; i<5; i++)
-	{
-		motor_back(100-20*i);
-		delay(100);
-	}
+	delay(2000);
+	/*
+	   for(i=2; i<5; i++)
+	   {
+	        motor_back(100-20*i);
+	        delay(100);
+	   }*/
 	motor_stop();
 	delay(100);
+
 }
