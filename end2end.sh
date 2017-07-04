2038996fd0e7fae36420114e142001d6b3d94c34
arahatashun <arahatashun@gmail.com>
2017-07-04 13:41:25 +0900

    debiug

--- end2end.sh
+++ end2end.sh
@@ -5,6 +5,6 @@ echo "start gut cut"
 sudo ./test_program/gut.out
 echo "start ground run"
 echo "Executing, ground_compass.out"
-sudo stdbuf -o0 -e0 ./ground_compass.out | tee log/`date +%Y%m%d_%H-%M-%S`.txt
+sudo stdbuf -o0 -e0 ./ground_compass.out | tee /home/pi/Documents/`date +%Y%m%d_%H-%M-%S`.txt
 echo "Executing, goal.out"
-sudo stdbuf -o0 -e0 ./object_recognition/goal.out | tee log/`date +%Y%m%d_%H-%M-%S`.txt
+sudo stdbuf -o0 -e0 ./object_recognition/goal.out | tee /home/pi/Documents/`date +%Y%m%d_%H-%M-%S`.txt
--- log.sh
+++ log.sh
@@ -3,5 +3,5 @@
 echo "Enter program name (excluding .out)"
 read program
 echo "Executing, $program.out"
-sudo stdbuf -o0 -e0 ./$program.out | tee ~/Documents/`date +%Y%m%d_%H-%M-%S`.txt
+sudo stdbuf -o0 -e0 ./$program.out | tee /home/pi/Documents/`date +%Y%m%d_%H-%M-%S`.txt
 #permissionが与えられてるのかわからず
