9cf32cb014215d21de0e8b167cac8dc26fd04666
cagalliorb <hk912united7@gmail.com>
2017-07-04 16:13:30 +0900

    debug

---
+++ test_program/accl_test.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+#include <wiringPiI2C.h>
+#include "../acclgyro.h"
+
+int main()
+{
+	Accl accl_data;
+	acclGyro_initialize();
+	while(1)
+	{
+		readAccl(&accl_data);
+		printf("x_accl is %d\n",accl_data.acclX_scaled);
+		printf("y_accl is %d\n",accl_data.acclY_scaled);
+		printf("z_accl is %d\n",accl_data.acclZ_scaled);
+		isReverse();
+		delay(1000);
+	}
+
+	return 0;
+}
