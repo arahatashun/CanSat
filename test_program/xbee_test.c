5621b00bd1f891a12f8a3e3370945bc8b14cd034
arahatashun <arahatashun@gmail.com>
2017-07-05 17:36:17 +0900

    debug

--- test_program/xbee_test.c
+++ test_program/xbee_test.c
@@ -7,10 +7,12 @@ int main()
 {
   xbee_init();
   gps_init();
+  int i=0;
   while (1)
   {
     loc_t data;
     gps_location(&data);
+    xbeePrintf("COUNTER : %d",++i);
 		xbeePrintf("latitude:%f\nlongitude:%f\n", data.latitude, data.longitude);
     delay(1000);
   }
