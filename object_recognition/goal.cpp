e74fedfa3ccc81a6c055d912b50e98827edcd191
arahatashun <arahatashun@gmail.com>
2017-07-05 22:28:11 +0900

    debug

--- object_recognition/goal.cpp
+++ object_recognition/goal.cpp
@@ -13,17 +13,17 @@ static const int LEFT_MAX = -100;
 static const int RIGHT_MAX = 100;
 static const int CENTER_THRESHOLD = 30;//-30~30で直進するようにする
 static const double EXIST_THRESHOLD = 0.7;//ゴール存在判定 パーセンテージ
-static const int TIME_LIMIT = 360000;
+static const int TIME_LIMIT = 360000;//6分
 
 //TODO turn millisecondどんどん大きくしていくPI制御にする
 int main (void)
 {
-	int start_time  = millis();
-	int last_time = millis();
+	int startTime  = millis();
+	int lastTime = millis();
 	pwm_initialize();
-	while(last_time-start_time<TIME_LIMIT)
+	while(lastTime-startTime<TIME_LIMIT)
 	{
-
+		lastTime = millis();
 		cv::Mat red = Mred();
 		double count = countArea(red);
 		if(count < EXIST_THRESHOLD)
