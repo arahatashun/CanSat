befef5f638ab1d81cf992c540178a1c93b768417
arahatashun <arahatashun@gmail.com>
2017-07-07 07:59:41 +0900

    debug

--- object_recognition/goal.cpp
+++ object_recognition/goal.cpp
@@ -14,7 +14,7 @@ static const int LEFT_MAX = -100;
 static const int RIGHT_MAX = 100;
 static const int CENTER_THRESHOLD = 30;//-30~30で直進するようにする
 static const double EXIST_THRESHOLD = 0.1;//ゴール存在判定 パーセンテージ
-static const int TIME_LIMIT = 600;//10分
+static const int TIME_LIMIT = 900;//10分
 
 //TODO turn millisecondどんどん大きくしていくPI制御にする
 int main (void)
