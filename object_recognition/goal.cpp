fcf227aa3c664a6a33c1336f9ac434701258be52
arahatashun <arahatashun@gmail.com>
2017-07-07 07:56:45 +0900

    debug

--- object_recognition/goal.cpp
+++ object_recognition/goal.cpp
@@ -9,7 +9,7 @@
 #pragma comment(lib,"opencv_world320.lib")
 
 static const int ROTATE_POWER = 100;
-static const int ROTATE_MILLISECONDS = 200;
+static const int ROTATE_MILLISECONDS = 220;
 static const int LEFT_MAX = -100;
 static const int RIGHT_MAX = 100;
 static const int CENTER_THRESHOLD = 30;//-30~30で直進するようにする
