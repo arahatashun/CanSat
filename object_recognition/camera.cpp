371e94dda763fae1fb014443afc09ba8c2ae8fc0
新幡駿 <shun@Emma.local>
2017-08-01 02:06:14 +0900

    n

--- object_recognition/camera.cpp
+++ object_recognition/camera.cpp
@@ -1,61 +1,73 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <opencv2/opencv.hpp>
+#include <ctime>
+#include <iostream>
 #include <opencv2/highgui/highgui.hpp>
+
 #include "camera.hpp"
 
-#pragma comment(lib,"opencv_world320.lib")
+static const int WIDTH = 800;
+static const int HEIGHT = 600;
+static const int FPS = 10;
 
-static const int N = 256;//文字列の長さ
+static const std::string DIRECTORY_PATH = "/home/pi/Pictures/";//pathの先頭
+static const std::string FILE_EXTENTION = ".jpg";//拡張子
+
 static const int AOV = 62.2;//ANGLE OF VIEW
 //明度について
 static const int MAX_VALUE = 255;//明るさ最大
 static const int NO_VALUE = 0;//明るさ最小
 
-
-//時間を元にStringを作る
-char* makeTimeString(void)
+Camera::Camera()
 {
-	static char timeString[N];
+	if (!capture.isOpened())
+	{
+		std::cout<<"capture is note opened"<<endl;
+	}
+	capture.set(CV_CAP_PROP_FRAME_WIDTH,WIDTH);
+	capture.set(CV_CAP_PROP_FRAME_HEIGHT,HEIGHT);
+	capture.set(CV_CAP_PROP_FPS,FPS);
+}
+
+Camera::~Camera()
+{
+	capture.release();
+}
+
+Camera::takePhoto()
+{
+	makeTimePath();
+	cv::Mat frame;
+	do
+	{
+		capture>>frame;
+	} while(frame.empty());
+	input = frame;
+	imwrite(timePath+FILE_EXTENTION,src);
+}
+
+//時間を元にtimePathを作る
+int Camera::makeTimePath(void)
+{
 	time_t timer;//時刻を受け取る変数
 	struct tm *timeptr;//日時を集めた構造体ポインタ
+	char buffer[80];
 	time(&timer);//現在時刻の取得
 	timeptr = localtime(&timer);//ポインタ
-	strftime(timeString, N, "%Y%m%d-%H%M%S", timeptr);//日時を文字列に変換してｓに代入
-	return timeString;
+	strftime(buffer, sizeof(buffer), "%Y%m%d-%H%M%S", timeptr);//日時を文字列に変換してｓに代入
+	std::string str(buffer);
+	timePath = DIRECTORY_PATH+str;
+  return 0;
 }
 
-char* makeBinaryString(char *timeString)
+//ノイズ除去
+cv::Mat Camera::rmNoize(cv::Mat src)
 {
-	static char binaryString[N];
-	sprintf(binaryString, "%s%s",timeString,"Binary");
-	return binaryString;
+	cv::erode(src,src,cv::Mat(),cv::Point(-1, -1),10);//縮小処理
+	cv::dilate(src,src,cv::Mat(),cv::Point(-1, -1),25);//膨張処理
+	cv::erode(src,src,cv::Mat(),cv::Point(-1, -1),15);//縮小処理
+	return src;
 }
 
-//full_pathを作る
-char* makePath(char* name)
-{
-	static char full_path[N];//NOTE 自動変数をreturn するために使った. smartなやり方か?
-	char directry_path[] = "/home/pi/Pictures/";//pathの先頭
-	char file_extention[] = ".jpg";//拡張子
-	sprintf(full_path, "%s%s%s",directry_path,name,file_extention);
-	return full_path;
-}
-//写真をとる
-int takePhoto(const char* name)
-{
-	char full_command[N];
-	char front_command[] = "raspistill -o ";//command
-	sprintf(full_command, "%s%s", front_command,name);//コマンドの文字列をつなげる。
-	system(full_command);//raspistillで静止画を撮って日時を含むファイル名で保存。
-	printf("%s\n",full_command);
-	//NOTE system関数以外を使うべきか?
-	return 0;
-}
-
-//二値化画像を作成
-cv::Mat binarize(cv::Mat src)
+int Camera::binarize()
 {
 	cv::Mat hsv;
 	cv::Mat hsv_filtered15 ;//画像の初期化
@@ -66,61 +78,31 @@ cv::Mat binarize(cv::Mat src)
 	cv::inRange(hsv, cv::Scalar(0, 70, 60), cv::Scalar(2, 255, MAX_VALUE), hsv_filtered15);
 	cv::inRange(hsv, cv::Scalar(160, 70, 60), cv::Scalar(180, 255, MAX_VALUE), hsv_filtered180);
 	cv::add(hsv_filtered15,hsv_filtered180,hsv);
-	return hsv;
-}
-//ノイズ除去
-cv::Mat rmNoize(cv::Mat src)
-{
-	cv::erode(src,src,cv::Mat(),cv::Point(-1, -1),10);//縮小処理
-	cv::dilate(src,src,cv::Mat(),cv::Point(-1, -1),25);//膨張処理
-	cv::erode(src,src,cv::Mat(),cv::Point(-1, -1),15);//縮小処理
-	return src;
-}
-
-int saveBinary(cv::Mat src,char* path)
-{
-	cv::Mat binary_img;
-	cv::resize(src,binary_img,cv::Size(),0.25,0.25);
-	imwrite(path, src);
+	output =rmNoise(hsv)
+	imwrite(timePath+"BINARY"+FILE_EXTENTION,output);
 	return 0;
-}
-
-//写真を撮り画像処理する
-cv::Mat Mred(void)
-{
-	char* stime = makeTimeString();
-	char* sbtime = makeBinaryString(stime);
-	const char* path = makePath(stime);
-	takePhoto(path);
-	cv::Mat src = cv::imread(path);
-	cv::Mat hsv;
-	hsv = rmNoize(binarize(src));
-	//NOTE makePathはstaticを使ってるのでここに置くこと
-	char* bpath = makePath(sbtime);
-	saveBinary(hsv,bpath);
-	return hsv;
 }
 
 
 //二値化した画像から1の面積を抽出
-double countArea(cv::Mat src)
+double Camera::countArea()
 {
 
-	double Area = src.rows*src.cols;//全ピクセル数
+	double Area = output.rows*output.cols;//全ピクセル数
 	double redCount = 0; //赤色を認識したピクセルの数
-	redCount = cv::countNonZero(src);//赤色部分の面積を計算
+	redCount = cv::countNonZero(output);//赤色部分の面積を計算
 	double percentage = 0; //割合
 	percentage = (redCount / Area)*100;//割合を計算
-	printf("面積のPercentageは%f\n", percentage);
+	std::cout<<"面積のPercentageは%"+percentage<<endl;
 	return percentage;
 }
 
 //二値化画像のcenterを角度で返す
-double getCenter(cv::Mat src)
+double Camera::getCenter()
 {
-	cv::Moments mu = cv::moments(src, false);//重心の計算結果をmuに代入
+	cv::Moments mu = cv::moments(output, false);//重心の計算結果をmuに代入
 	double mc = mu.m10 / mu.m00;//重心のx座標
 	double center = (mc - src.cols / 2) * AOV / src.cols;//正規化
-	printf("重心の位置は%f\n",center);
+	std::cout<<"重心の位置は"+center;
 	return center;
 }
--- object_recognition/camera.hpp
+++ object_recognition/camera.hpp
@@ -1,8 +1,32 @@
 #ifndef CAMERA_H
 #define CAMERA_H
-#include <opencv2/opencv.hpp>
 
-cv::Mat Mred(void);
-double countArea(cv::Mat src);
-double getCenter(cv::Mat src);
+#include <string>
+#include <opencv2/opencv.hpp>
+#include <opencv/videoio.hpp>
+
+#pragma comment(lib,"opencv_world320.lib")
+
+static const int devid = 0;
+
+class Camera
+{
+private:
+  void operator=(cosnt Camera&){};
+  Camera(const Camera&){};
+  int makeTimePath();
+  cv::Mat Camera::rmNoize();
+  int Camera::binarize();
+  cv::VideoCapture capture(0);
+  string timePath;
+  cv::Mat input;//入力画像
+  cv::Mat output;//出力画像
+public:
+  Camera();
+  ~Camera();
+  int takePhoto();
+  int binarize();
+  double countArea();
+  double getCenter();
+}
 #endif
