63e8fbe182e6f3509c717d6b5362badea7e2ee6c
cagalliorb <hk912united7@gmail.com>
2017-08-02 14:22:29 +0900

    debug

--- acclgyro.h
+++ acclgyro.h
@@ -5,21 +5,6 @@
 extern "C" {
 #endif
 
-//三軸加速度(単位G)
-typedef struct accl {
-  double acclX_scaled;//the values of accleration
-  double acclY_scaled;
-  double acclZ_scaled;
-}Accl;
-
-//角速度 degrees/second
-//NOTE z軸が上なので基本的にそれをみると良い
-typedef struct gyro {
-  double gyroX_scaled;//the values of accleration
-  double gyroY_scaled;
-  double gyroZ_scaled;
-}Gyro;
-
 int acclGyro_initialize(void);
 int Gyro_read(Gyro *data);
 int Accl_read(Accl*data);
