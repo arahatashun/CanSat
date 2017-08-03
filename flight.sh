0a75b8b8d6c6ba20c2650a8c5bec3722587dc7ee
cagalliorb <hk912united7@gmail.com>
2017-08-03 18:20:52 +0900

    debug

--- flight.sh
+++ flight.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #NOTE 事前に実行ファイルの用意
 trap 'echo "SIGIN Handle"; sudo ./sigintHandle.out; exit 1' 2
-echo "flight sequence start
+echo "flight sequence start"
 sudo stdbuf -o0 -e0 ./flight.out | tee /home/pi/Documents/`date +%Y%m%d_%H-%M-%S`.txt
