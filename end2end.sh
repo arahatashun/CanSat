eb355019c501776850f4d50162743bee63df0abf
cagalliorb <hk912united7@gmail.com>
2017-08-07 00:18:23 +0900

    flight.cも追加

--- end2end.sh
+++ end2end.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 #NOTE 事前に実行ファイルの用意
 trap 'echo "SIGIN Handle"; sudo ./sigintHandle.out; exit 1' 2
+echo "start flight sequence"
+sudo stdbuf -o0 -e0 ./flight.out | tee /home/pi/Documents/`date +%Y%m%d_%H-%M-%S`.txt
 echo "start gut cut"
 sudo ./test_program/gut.out
 echo "start forward"
 sudo ./forward.out
-echo "start ground run"
+echo "start ground running sequence"
 echo "Executing, ground_compass.out"
 sudo stdbuf -o0 -e0 ./ground_compass.out | tee /home/pi/Documents/`date +%Y%m%d_%H-%M-%S`.txt
 echo "Executing, goal.out"
