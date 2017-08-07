26507704c9accb9263b94cab26d846eca5143848
cagalliorb <hk912united7@gmail.com>
2017-08-07 13:21:42 +0900

    ednefjoae

--- end2end.sh
+++ end2end.sh
@@ -3,8 +3,6 @@
 trap 'echo "SIGIN Handle"; sudo ./sigintHandle.out; exit 1' 2
 echo "start flight sequence"
 sudo stdbuf -o0 -e0 ./flight.out | tee /home/pi/Documents/`date +%Y%m%d_%H-%M-%S`.txt
-echo "start gut cut"
-sudo ./test_program/gut.out
 echo "start forward"
 sudo ./forward.out
 echo "start ground running sequence"
