5dba11f22f6fd363137a2e54baff77ae724e0279
cagalliorb <hk912united7@gmail.com>
2017-08-07 21:13:08 +0900

    debug

--- motor.c
+++ motor.c
@@ -137,10 +137,9 @@ int motor_rotate_compass(double angle_to_rotate)
 	double target_angle = cal_deviated_angle(0, compass_angle_fixed + angle_to_rotate);
 	while(fabs(delta_angle) > 30)
 	{
-		printf("%f\n", fabs(delta_angle));
 		double compass_angle =readCompassAngle();
 		printf("compass_angle: %f\n", compass_angle);
-		double delta_angle= cal_delta_angle(compass_angle,target_angle);
+		delta_angle= cal_delta_angle(compass_angle,target_angle);
 		printf("delta_angle: %f\n", delta_angle);
 		if(delta_angle>0)
 		{
