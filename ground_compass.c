ac3880285177e0666c71bd5d3488fa803fca2d98
cagalliorb <hk912united7@gmail.com>
2017-08-12 15:43:25 +0900

    add motor_rotate

--- ground_compass.c
+++ ground_compass.c
@@ -82,6 +82,46 @@ int updateCoord(Queue* latring,Queue* lonring)
 	return 0;
 }
 
+int motor_rotate_compass(double angle_to_rotate)
+{
+	int c = 0;  //行きたい方角に回転できなくても無限ループにならないようにカウンター用意
+	double delta_angle = 180;
+	double compass_angle_fixed =readCompassAngle();
+	double target_angle = cal_deviated_angle(0, compass_angle_fixed + angle_to_rotate);
+	while(fabs(delta_angle) > 30 && c <STACK_COUNTER)
+	{
+		double compass_angle =readCompassAngle();
+		delta_angle= cal_delta_angle(compass_angle,target_angle);
+		printf("delta_angle: %f\n", delta_angle);
+		if(delta_angle>0)
+		{
+			motor_right(100);
+		}
+		else
+		{
+			motor_left(100);
+		}
+		delay(100);
+		c++;
+	}
+	if(c >= STACK_COUNTER)
+	{
+		printf("could not escape\n");
+	}
+
+	motor_stop();
+	delay(2000);
+	return 0;
+}
+
+int motor_escape()
+{
+	printf("get stacked\n");
+	motor_rotate_compass(90);
+	motor_rotate_compass(-90);
+	return 0;
+}
+
 //スタック判定をして抜け出す処理まで
 int stackJudge(Queue* latring,Queue* lonring)
 {
