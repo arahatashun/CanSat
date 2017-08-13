ab49c54e2ac95608a9a89d65e04807bbbae576b7
Shun Arahata <arahatashun@users.noreply.github.com>
2017-08-14 00:01:17 +0900

    debug

--- test_program/reboot.c
+++ test_program/reboot.c
@@ -1,9 +1,15 @@
 #include <unistd.h>
 #include <linux/reboot.h>
+#include <errno.h>
 
 int main()
 {
 	sync();
-	reboot(LINUX_REBOOT_CMD_RESTART);
+	if(reboot(LINUX_REBOOT_CMD_RESTART)== -1)
+	{
+		printf("WARNING! REBOOT FAILED\n");
+		printf("errno=%d: %s\n",errno, strerror(errno));
+		return -1;
+	}
 	return 0;
 }
