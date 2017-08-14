0ec97ae8569267a445677e634d9b617403815810
Shun Arahata <arahatashun@users.noreply.github.com>
2017-08-14 17:18:08 +0900

    debug

--- object_recognition/camera.cpp
+++ object_recognition/camera.cpp
@@ -74,9 +74,9 @@ int Camera::makeTimePath(void)
 cv::Mat Camera::rmNoise(cv::Mat src)
 {
 	std::vector<std::vector<cv::Point> > contours; //輪郭座標の二次元配列
-	cv::findContours(src, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE); //CV_RETR_EXTERNAL:最も外側の輪郭を検出、CV_CHAIN_APPROX_NONE:近似手法
- 	std::vector<std::vector<cv::Point> > contours_subset;
- 	for (int i = 0; i<contours.size(); i++)  
+	cv::findContours(src,contours,CV_RETR_EXTERNAL,CV_CHAIN_APPROX_NONE); //CV_RETR_EXTERNAL:最も外側の輪郭を検出、CV_CHAIN_APPROX_NONE:近似手法
+ 	std::vector<std::vector<cv::Point>> contours_subset;
+ 	for (int i = 0;i<contours.size();i++)  
 	{  
 		double area = cv::contourArea(contours.at(i)); //各輪郭の面積 
 		if (area>MIN_AREA)   
@@ -92,13 +92,13 @@ cv::Mat Camera::rmNoise(cv::Mat src)
 int Camera::binarize()
 {
 	cv::Mat hsv;
-	cv::Mat hsv_filtered15 ;//画像の初期化
+	cv::Mat hsv_filtered15;//画像の初期化
 	cv::Mat hsv_filtered180;//画像の初期化
-	cv::cvtColor(input, hsv, CV_BGR2HSV);//入力画像(src)をhsv色空間(dst)に変換
+	cv::cvtColor(input,hsv,CV_BGR2HSV);//入力画像(src)をhsv色空間(dst)に変換
 	//inRange(入力画像,下界画像,上界画像,出力画像)
 	//「HSV」は、色を色相(Hue)・彩度(Saturation)・明度(Value)
-	cv::inRange(hsv, cv::Scalar(0, 70, 60), cv::Scalar(2, 255, MAX_VALUE), hsv_filtered15);
-	cv::inRange(hsv, cv::Scalar(160, 70, 60), cv::Scalar(180, 255, MAX_VALUE), hsv_filtered180);
+	cv::inRange(hsv,cv::Scalar(0,70,60),cv::Scalar(2,255,MAX_VALUE),hsv_filtered15);
+	cv::inRange(hsv,cv::Scalar(160,70,60),cv::Scalar(180,255,MAX_VALUE),hsv_filtered180);
 	cv::add(hsv_filtered15,hsv_filtered180,hsv);
 	output = rmNoise(hsv);
 	imwrite(timePath+"BINARY"+FILE_EXTENTION,output);
