54f7c9d08c31eccdc4ac57e105da283769c24356
Shun Arahata <arahatashun@users.noreply.github.com>
2017-08-18 07:25:23 +0900

    debug

--- mitibiki.c
+++ mitibiki.c
@@ -5,8 +5,8 @@
 //note: seikei toukei ni izon
 
 
-static const double target_latitude = 35.760317;//緯度
-static const double target_longitude = 139.766030;//経度
+static const double target_latitude = 40.142338;//緯度
+static const double target_longitude = 139.987257;//経度
 static const double PI = 3.14159265;
 static const double EARTH_RADIUS = 6378137;
 
