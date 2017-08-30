fbcd6d435c6a7f4fcd69eb98788d5ca6f891af05
Shun Arahata <arahatashun@users.noreply.github.com>
2017-08-30 11:14:08 +0900

    dbeug

--- test_program/bme280.sh
+++ test_program/bme280.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 echo "Executing, bme280.out"
-sudo stdbuf -o0 -e0 ./bme280.out | tee /home/pi/Documents/`date +%Y%m%d_%H-%M-%S`.txt
+sudo stdbuf -o0 -e0 ./bme280.out | tee /home/pi/Documents/bme280`date +%Y%m%d_%H-%M-%S`.txt
 #permissionが与えられてるのかわからず
--- test_program/gps.sh
+++ test_program/gps.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 echo "Executing, gps.out"
-sudo stdbuf -o0 -e0 ./gps.out | tee /home/pi/Documents/`date +%Y%m%d_%H-%M-%S`.txt
+sudo stdbuf -o0 -e0 ./gps.out | tee /home/pi/Documents/gps`date +%Y%m%d_%H-%M-%S`.txt
 #permissionが与えられてるのかわからず
