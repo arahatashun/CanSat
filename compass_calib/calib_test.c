86a585653efba21de18bb2e47fb1828721dc1954
cagalliorb <hk912united7@gmail.com>
2017-09-09 22:50:30 -0700

    a

--- compass_calib/calib_test.c
+++ compass_calib/calib_test.c
@@ -1,15 +1,24 @@
 /*マシンを自動で回転させながら地磁気のログを取るプログラム
    今のところ使う予定はないが、一応残しておく*/
 #include <wiringPi.h>
- #include "../compass.h"
- #include <stdio.h>
+#include "../compass.h"
+#include <stdio.h>
+#include <signal.h>
+
+void handler(int signum)
+{
+	motor_stop();
+	delay(100);
+	exit(1);
+}
 
 int main()
 {
 	pwm_initialize();
 	compass_initialize();
-    double x_fuck = 0;
-    double y_fuck = 0;
+	signal(SIGINT, handler);
+        double x_fuck = 0;
+        double y_fuck = 0;
 	cal_maxmin_compass(&x_fuck, &y_fuck);
 	delay(2000);
 	printf("compassx_offset = %f\n", x_fuck);
