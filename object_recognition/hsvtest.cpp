9a4b1e99fc0d834fdb97d068ba0b4b2754f1c54e
Shun Arahata <arahatashun@users.noreply.github.com>
2017-09-11 00:18:43 -0700

    deug

--- object_recognition/hsvtest.cpp
+++ object_recognition/hsvtest.cpp
@@ -18,8 +18,8 @@ int main()
     cv::cvtColor(input,hsv,CV_BGR2HSV);//入力画像(src)をhsv色空間(dst)に変換
     //inRange(入力画像,下界画像,上界画像,出力画像)
     //「HSV」は、色を色相(Hue)・彩度(Saturation)・明度(Value)
-    cv::inRange(hsv,cv::Scalar(0,65,70),cv::Scalar(10,255,255),hsv_filtered15);
-    cv::inRange(hsv,cv::Scalar(175,65,70),cv::Scalar(180,255,255),hsv_filtered180);
+    cv::inRange(hsv,cv::Scalar(0,80,70),cv::Scalar(10,255,255),hsv_filtered15);
+    cv::inRange(hsv,cv::Scalar(175,80,70),cv::Scalar(180,255,255),hsv_filtered180);
     cv::add(hsv_filtered15,hsv_filtered180,output);
     cv::imwrite(path+"BINARY"+".jpg",output);
   }
